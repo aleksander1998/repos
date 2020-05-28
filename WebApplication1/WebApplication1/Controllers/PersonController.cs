2820e1dbb2a58599109014cd3fa0c02ecd2955ee
Aleksander <aleksandern@getacademy.no>
2020-05-28 12:28:44 +0200

    d
    d

---
+++ WebApplication1/WebApplication1/Controllers/PersonController.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using WebApplication1.Model;
+
+namespace WebApplication1.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PersonController : ControllerBase
+    {
+        private Person[] _data = new[]
+        {
+            new Person() {Id = 1, Email = "Pipondu@nuoon.net", Name = "Pipondu"},
+            new Person() {Id = 2, Email = "Suri@nuoon.net", Name = "Suri"},
+            new Person() {Id = 3, Email = "Windo@nuoon.net", Name = "Windo"}
+        };
+
+        [HttpGet("{id}")]
+        public async Task <Person> GetOne(int id)
+        {
+            return await Task.Run(() => 
+               _data.SingleOrDefault(p=>p.Id==id)
+            );
+        }
+        
+
+        [HttpGet]
+        public async Task <IEnumerable <Person>> GetMany ()
+        {
+            return await Task.Run(() => _data);
+            
+        }
+        [HttpPost]
+        public async  Task <int> Create(Person person)
+        {
+            return await Task.Run(() => 1);
+        }
+
+        [HttpPut]
+        public async Task<int> Edit(Person person)
+        {
+            return await Task.Run(() => 1);
+        }
+
+
+        [HttpDelete]
+        public async Task <int>  Delete(int id)
+        {
+            return await Task.Run(() => 1) ;
+        }
+    }
+}
---
+++ WebApplication1/WebApplication1/Model/Person.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.PortableExecutable;
+using System.Threading.Tasks;
+
+namespace WebApplication1.Model
+{
+    public class Person
+    {
+        public int Id { get; set; }
+        public  string Name { get; set;  }
+        public  string Email { get; set; }
+
+        }
+}
